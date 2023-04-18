cd59b2d65ebd52a3a8e20a8182cf547eb5ab5eae
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-04-18 11:28:09 +0200

    Initial Commit

---
+++ HabitRabbit/HabitRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitRabbitApp.swift
+//  HabitRabbit
+//
+//  Created by Julia Petersson  on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct HabitRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
