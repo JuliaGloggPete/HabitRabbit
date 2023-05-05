bfd3e19f2fb9593694ea8583d7128b09daabef17
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-05-05 07:31:31 +0200

    carrots to cheer you up

---
+++ HabitRabbit/InfoOverlayView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoOverlayView.swift
+//  HabitRabbit
+//
+//  Created by Julia Petersson  on 2023-05-04.
+//
+
+import SwiftUI
+
+struct InfoOverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoOverlayView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoOverlayView()
+    }
+}
